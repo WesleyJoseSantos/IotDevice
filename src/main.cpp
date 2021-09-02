b3233b3ebb6df1ce9229597102409306e72073f6
WESLEY JOSE SANTOS <53940899+WesleyJoseSantos@users.noreply.github.com>
2021-09-02 10:08:59 -0300

    feat: implement basic main file

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,19 @@
 #include <Arduino.h>
+#include "Prov/Prov.h"
+#include "IotDevice.hpp"
+
+IotDevice device(Serial);
+Prov prov(device, "Prov AP", 30000);
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(74400);
+
+  device.init();
+  prov.begin("/prov.json", "/prov.html"); 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  prov.task();
 }
